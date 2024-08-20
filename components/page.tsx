d04ce2169bcba370232f8c84636bf7661b103e0e
Linhtranne <147390264+Linhtranne@users.noreply.github.com>
2024-08-20 08:12:26 +0700

    ss6-7

---
+++ components/page.tsx
@@ -0,0 +1,10 @@
+import { log } from 'console'
+import React from 'react'
+
+export default function Dashboard() {
+    console.log("gia tri props", props);
+    
+  return (
+    <div>Dashboard</div>
+  )
+}
