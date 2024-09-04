3338d27e05bcdae01d56f595d7e1edf7add4f041
Linhtranne <147390264+Linhtranne@users.noreply.github.com>
2024-09-04 11:34:43 +0700

    Update page.tsx

--- components/page.tsx
+++ components/page.tsx
@@ -1,7 +1,7 @@
 import { log } from 'console'
 import React from 'react'
 
-export default function Dashboard() {
+export default function Dashboard(props: any)  {
     console.log("gia tri props", props);
     
   return (
